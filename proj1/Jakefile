5da82a003ad049c4976aca2739649730a3142a29
Kurtis <kurtis@kurtis.(none)>
2013-02-10 12:44:35 -0800

    fast commit does not care for comments

---
+++ proj1/Jakefile
@@ -0,0 +1,6 @@
+
+var t = new jake.TestTask('proj1', function () {
+  this.testFiles.include('test/*.js');
+  this.testFiles.include('test/**/*.js');
+});
+
---
+++ proj1/app/controllers/logins.js
@@ -0,0 +1,11 @@
+var Logins = function () {
+  this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
+
+  this.index = function (req, resp, params) {
+    this.respond({params: params});
+  };
+
+};
+
+exports.Logins = Logins;
+
