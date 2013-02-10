16160fca1be78ada54de1c3950be7e9eb0ed1599
Kurtis <kurtis@kurtis.(none)>
2013-02-09 23:16:23 -0800

    blah

--- web.js
+++ web.js
@@ -20,23 +20,26 @@ app.get('/user', function(request, response){
 });
 
 app.post('/users/login', function(request, response){
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		response.send("request\n"+request.headers);
-		var query = client.query('SELECT count FROM users u WHERE u.password = ' + request.password + ' and u.name = ' + request.user);
-		var returnDict = {};
-		query.on('row', function(result)
-		{
-			//response.send("test\n"+console.dir(result));
-			if(!result)
+	request.addListener("data", function(data){
+		response.send("data is: "+data);
+		pg.connect(process.env.DATABASE_URL, function(err, client) {
+			request.send(data.password);
+			var query = client.query('SELECT count FROM users u WHERE u.password = ' + data.password + ' and u.name = ' + data.user);
+			var returnDict = {};
+			query.on('row', function(result)
 			{
-				returnDict = {"errCode":-1};
-			}
-			else
-			{
-				returnDict = {"errCode":1, "count":result};
-			}	
+				//response.send("test\n"+console.dir(result));
+				if(!result)
+				{
+					returnDict = {"errCode":-1};
+				}
+				else
+				{
+					returnDict = {"errCode":1, "count":result};
+				}	
+			});
+			response.send(returnDict);
 		});
-		response.send(returnDict);
 	});
 });
 // app.post('/users/add', function(request, response){
