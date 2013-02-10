ef507e7323692ded7a6e8c58f19227df80605f96
Kurtis <kurtis@kurtis.(none)>
2013-02-09 22:21:21 -0800

    blah

--- web.js
+++ web.js
@@ -11,6 +11,16 @@ app.get('/', function(request, response) {
 
 app.post('/users/login', function(request, response){
 	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		var query = client.query('SELECT * from users')
+		query.on('row', function(result)
+		{
+			response.send(result);
+		});
+	});
+});
+
+app.post('/users/login', function(request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
 		var query = client.query('SELECT count FROM users u WHERE u.password = ' + request.password + ' and u.name = ' + request.user);
 		var returnDict = {};
 		query.on('row', function(result)
