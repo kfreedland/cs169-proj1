dba3d516110e1569290f251b6f610b9b38a42e2a
Kurtis <kurtis@kurtis.(none)>
2013-02-11 14:31:08 -0800

    fast commit does not care for comments

--- test/user.js
+++ test/user.js
@@ -40,18 +40,21 @@ tests = {
 
     'addInvalidUName': function ()
     {
+        console.log("null Uname");
     	User.add(null, 'shouldnt matter', function (responseDict)
     	{
     		//ERR_BAD_USERNAME
     		assert.equal(responseDict, {'errCode': -3});
     	});
 
+        console.log("empty Uname");
     	User.add('','shouldnt matter', function (responseDict)
     	{
     		//ERR_BAD_USERNAME
     		assert.equal(responseDict, {'errCode':--3});
     	});
 
+        console.log("long Uname");
     	User.add(longUser, 'shouldnt matter', function (responseDict)
     	{
     		//ERR_BAD_USERNAME
@@ -61,18 +64,21 @@ tests = {
 
     'addInvalidPword' : function ()
     {
+        console.log("null pword");
     	User.add('fail1', null, function (responseDict)
     	{
     		//ERR_BAD_PASSWORD
     		assert.equal(responseDict, {'errCode': -4});
     	});
 
+        console.log("empty pword");
     	User.add('fail2', '', function (responseDict)
     	{
     		//ERR_BAD_PASSWORD
     		assert.equal(responseDict, {'errCode': -4});
     	});
 
+        console.log("long pword");
     	User.add('fail3', longPass, function(responseDict)
     	{
     		//ERR_BAD_PASSWORD
