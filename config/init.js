6e045776bfc772756b035f784f0551a7cfdff215
Kurtis <kurtis@kurtis.(none)>
2013-02-10 23:26:18 -0800

    fast commit does not care for comments

--- config/init.js
+++ config/init.js
@@ -1,4 +1,8 @@
-
+//Create connection to database
+var pg = require('pg');
+// var qs = require('querystring');
+var client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 // Add uncaught-exception handler in prod-like environments
 if (geddy.config.environment != 'development') {
   process.addListener('uncaughtException', function (err) {
