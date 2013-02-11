183b4993aa5259c9acdd5152a98a558aa872cf7d
Kurtis <kurtis@kurtis.(none)>
2013-02-11 10:36:14 -0800

    fast commit does not care for comments

--- config/production.js
+++ config/production.js
@@ -40,7 +40,7 @@ var config = {
   }
 , db: {
     postgres: {
-      user: 'gnabxzjzpghah'
+      user: 'gnabxzjzpghahz'
     , database: 'd6aps2hl4f0hjo'
     , password: 'EzNNLx9XSDD-ssX5CarFH41x0X'
     , host: 'ec2-54-243-235-7.compute-1.amazonaws.com'
