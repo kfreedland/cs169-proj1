c1e8252dcfe5be911151d06f6e36242b6b65fb82
Kurtis <kurtis@kurtis.(none)>
2013-02-11 19:19:58 -0800

    fast commit does not care for comments

--- test/user.js
+++ test/user.js
@@ -25,86 +25,86 @@ tests = {
             });
 	           
   		});
-    },
-
-    'addUserB': function ()
-    {
-        console.log("adding second user");
-  		User.add('bUser','bPassword', function (responseDict)
-		{
-			//SUCCESS
-	    	assert.deepEqual(responseDict, {'errCode':1, 'count': 1});
-	    
-  		});
-    },
-
-    'addExistingUser': function ()
-    {
-        User.add('cUser','cPassword', function (responseDict1)
-        {
-            User.add('cUser','cPassword', function (responseDict)
-            {
-                //ERR_USER_EXISTS
-                assert.equal(responseDict, {'errCode':-2});
-            });
-        });
-    },
-
-    'addInvalidUName': function ()
-    {
-        console.log("null Uname");
-    	User.add(null, 'shouldnt matter', function (responseDict)
-    	{
-    		//ERR_BAD_USERNAME
-    		assert.deepEqual(responseDict, {'errCode': -3});
-    	});
-
-        console.log("empty Uname");
-    	User.add('','shouldnt matter', function (responseDict)
-    	{
-    		//ERR_BAD_USERNAME
-    		assert.deepEqual(responseDict, {'errCode':--3});
-    	});
-
-        console.log("long Uname");
-    	User.add(longUser, 'shouldnt matter', function (responseDict)
-    	{
-    		//ERR_BAD_USERNAME
-    		assert.deepEqual(responseDict, {'errCode': -3});
-    	});
-    },
-
-    'addInvalidPword' : function ()
-    {
-        console.log("null pword");
-    	User.add('fail1', null, function (responseDict)
-    	{
-    		//ERR_BAD_PASSWORD
-    		assert.deepEqual(responseDict, {'errCode': -4});
-    	});
-
-        console.log("empty pword");
-    	User.add('fail2', '', function (responseDict)
-    	{
-    		//ERR_BAD_PASSWORD
-    		assert.deepEqual(responseDict, {'errCode': -4});
-    	});
-
-        console.log("long pword");
-    	User.add('fail3', longPass, function(responseDict)
-    	{
-    		//ERR_BAD_PASSWORD
-    		assert.deepEqual(responseDict, {'errCode': -4});
-    	});
-	},
-
-    'deleteAll' : function ()
-    {
-        User.resetFixture(function (responseDict)
-        {
-            assert.deepEqual(responseDict, {'errCode': 1});
-        })
     }
+
+ //    'addUserB': function ()
+ //    {
+ //        console.log("adding second user");
+ //  		User.add('bUser','bPassword', function (responseDict)
+	// 	{
+	// 		//SUCCESS
+	//     	assert.deepEqual(responseDict, {'errCode':1, 'count': 1});
+	    
+ //  		});
+ //    },
+
+ //    'addExistingUser': function ()
+ //    {
+ //        User.add('cUser','cPassword', function (responseDict1)
+ //        {
+ //            User.add('cUser','cPassword', function (responseDict)
+ //            {
+ //                //ERR_USER_EXISTS
+ //                assert.equal(responseDict, {'errCode':-2});
+ //            });
+ //        });
+ //    },
+
+ //    'addInvalidUName': function ()
+ //    {
+ //        console.log("null Uname");
+ //    	User.add(null, 'shouldnt matter', function (responseDict)
+ //    	{
+ //    		//ERR_BAD_USERNAME
+ //    		assert.deepEqual(responseDict, {'errCode': -3});
+ //    	});
+
+ //        console.log("empty Uname");
+ //    	User.add('','shouldnt matter', function (responseDict)
+ //    	{
+ //    		//ERR_BAD_USERNAME
+ //    		assert.deepEqual(responseDict, {'errCode':--3});
+ //    	});
+
+ //        console.log("long Uname");
+ //    	User.add(longUser, 'shouldnt matter', function (responseDict)
+ //    	{
+ //    		//ERR_BAD_USERNAME
+ //    		assert.deepEqual(responseDict, {'errCode': -3});
+ //    	});
+ //    },
+
+ //    'addInvalidPword' : function ()
+ //    {
+ //        console.log("null pword");
+ //    	User.add('fail1', null, function (responseDict)
+ //    	{
+ //    		//ERR_BAD_PASSWORD
+ //    		assert.deepEqual(responseDict, {'errCode': -4});
+ //    	});
+
+ //        console.log("empty pword");
+ //    	User.add('fail2', '', function (responseDict)
+ //    	{
+ //    		//ERR_BAD_PASSWORD
+ //    		assert.deepEqual(responseDict, {'errCode': -4});
+ //    	});
+
+ //        console.log("long pword");
+ //    	User.add('fail3', longPass, function(responseDict)
+ //    	{
+ //    		//ERR_BAD_PASSWORD
+ //    		assert.deepEqual(responseDict, {'errCode': -4});
+ //    	});
+	// },
+
+ //    'deleteAll' : function ()
+ //    {
+ //        User.resetFixture(function (responseDict)
+ //        {
+ //            assert.deepEqual(responseDict, {'errCode': 1});
+ //        })
+ //    }
 
 };
 
