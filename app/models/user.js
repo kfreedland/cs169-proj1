813644346ac0dd16b3815c02aa28dff4ef0bbd48
Kurtis <kurtis@kurtis.(none)>
2013-02-12 16:05:09 -0800

    fast commit does not care for comments

--- app/models/user.js
+++ app/models/user.js
@@ -136,7 +136,7 @@ User.unitTests = function TESTAPI_unitTests(callback)
   var testArr = new Array();
   for (var i in tests)
   {
-    console.log("TEST: "+test[i]);
+    console.log("TEST: "+tests[i]);
     testArr[length] = tests[i]; 
     length++;
   }
